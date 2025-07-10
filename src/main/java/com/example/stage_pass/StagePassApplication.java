441b05aade054cd1d2d13b3427022e4b7c895e22
son0925 <shs00925@naver.com>
2025-07-10 16:03:36 +0900

    chore/config

---
+++ src/main/java/com/example/stage_pass/StagePassApplication.java
@@ -0,0 +1,13 @@
+package com.example.stage_pass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StagePassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StagePassApplication.class, args);
+	}
+
+}
